658166260a9ec2183b6aa675b40e5f123ac2af43
Amir Jann Castaneda <amirj.castaneda@gmail.com>
2021-12-27 06:55:52 +0800

    Add Intermediate Algorithm Scripting solutions

---
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/13-sum-all-primes.js
@@ -0,0 +1,23 @@
+// Sum All Primes
+// A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+// Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+
+
+function sumPrimes(num) {
+  let sum = 0;
+  for (let currentValue=2; currentValue <= num; currentValue++) {
+    if (isPrime(currentValue)) 
+      sum += currentValue;
+  }
+  return sum;
+
+  function isPrime(arg) {
+    for (let i = 2; i < arg; i++) {
+      if (arg % i === 0) 
+        return false;
+    }
+    return true;
+  }
+}
+
+sumPrimes(10);
---
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/14-smallest-common-multiple.js
@@ -0,0 +1,28 @@
+// Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+// The range will be an array of two numbers that will not necessarily be in numerical order.
+// For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
+
+
+function smallestCommons(arr) {
+  arr.sort((a,b) => a < b ? -1 : 1);
+  let num1 = arr[0];
+  let num2 = arr[1];
+  while (true) {
+    if (!checkDivisibility(num2) || num2 % num1 != 0) {
+      num2 += arr[1];
+    }
+    else {
+      break;
+    }
+  }
+  function checkDivisibility(num) {
+    for (let i=arr[0]; i <= arr[1]; i++) {
+      if (num % i !== 0)
+        return false;
+    }
+    return true;
+  }
+  return num2;
+}
+
+console.log(smallestCommons([1,13]));
